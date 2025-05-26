970a76faca7b2199688e64ca0054f2693dc07822
Eduardo Gatti <eduardo.gf@hotmail.com>
2025-05-26 15:16:09 -0600

    checkemail

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,17 +1,17 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 
-import { CurrentUser } from './current-user.decorator';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { MeDto } from './dto/me.dto';
-import { UseGuards } from '@nestjs/common';
-import { User } from '../../@generated/user/user.model';
-import { UserService } from './user.service';
-import { UserUpdateInput } from '../../@generated/user/user-update.input';
-import { SearchDto } from './dto/search.dto';
-import { PaginationArgs } from '../shared/types/pagination.input';
-import { UserCreateInput } from '../../@generated/user/user-create.input';
-import { CreateUserWithoutPassword } from './dto/createUser.dto';
-import { BaseResult } from '../models/base-error.dto';
+import { CurrentUser } from "./current-user.decorator";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
+import { MeDto } from "./dto/me.dto";
+import { UseGuards } from "@nestjs/common";
+import { User } from "../../@generated/user/user.model";
+import { UserService } from "./user.service";
+import { UserUpdateInput } from "../../@generated/user/user-update.input";
+import { SearchDto } from "./dto/search.dto";
+import { PaginationArgs } from "../shared/types/pagination.input";
+import { UserCreateInput } from "../../@generated/user/user-create.input";
+import { CreateUserWithoutPassword } from "./dto/createUser.dto";
+import { BaseResult } from "../models/base-error.dto";
 
 @Resolver()
 export class UserResolver {
@@ -23,22 +23,31 @@ export class UserResolver {
     return this.userService.users();
   }
 
-  @Query(() => MeDto, { name: 'me' })
+  @Query(() => MeDto, { name: "me" })
   @UseGuards(JwtAuthGuard)
   me(@CurrentUser() currentUser: User) {
     return this.userService.me(currentUser);
   }
 
-  @Mutation(() => User, { name: 'createUser' })
-  createUser(@Args('data') data: UserCreateInput) {
+  @Query(() => Boolean, { name: "emailAvailable" })
+  async isEmailAvailable(@Args("email") email: string): Promise<boolean> {
+    const user = await this.userService.user({
+      where: { email },
+    });
+
+    return !user; 
+  }
+
+  @Mutation(() => User, { name: "createUser" })
+  createUser(@Args("data") data: UserCreateInput) {
     return this.userService.createUser(data);
   }
 
-  @Mutation(() => User, { name: 'updateUser' })
+  @Mutation(() => User, { name: "updateUser" })
   @UseGuards(JwtAuthGuard)
   updateUser(
     @CurrentUser() currentUser: User,
-    @Args('data') data: UserUpdateInput,
+    @Args("data") data: UserUpdateInput
   ) {
     return this.userService.updateUser(currentUser, data);
   }
@@ -47,36 +56,36 @@ export class UserResolver {
   @UseGuards(JwtAuthGuard)
   search(
     @CurrentUser() currentUser: User,
-    @Args('term', { nullable: true }) term?: string,
-    @Args('userId', { nullable: true }) userId?: number,
-    @Args('pagination', { nullable: true }) pagination?: PaginationArgs,
+    @Args("term", { nullable: true }) term?: string,
+    @Args("userId", { nullable: true }) userId?: number,
+    @Args("pagination", { nullable: true }) pagination?: PaginationArgs
   ) {
     return this.userService.search(currentUser, pagination, term, userId);
   }
 
-  @Mutation(() => Boolean, { name: 'followUser' })
+  @Mutation(() => Boolean, { name: "followUser" })
   @UseGuards(JwtAuthGuard)
-  followUser(@Args('userId') userId: number, @CurrentUser() currentUser: User) {
+  followUser(@Args("userId") userId: number, @CurrentUser() currentUser: User) {
     return this.userService.followUser(userId, currentUser.id);
   }
 
-  @Mutation(() => Boolean, { name: 'unfollowUser' })
+  @Mutation(() => Boolean, { name: "unfollowUser" })
   @UseGuards(JwtAuthGuard)
   unfollowUser(
-    @Args('userId') userId: number,
-    @CurrentUser() currentUser: User,
+    @Args("userId") userId: number,
+    @CurrentUser() currentUser: User
   ) {
     return this.userService.unfollowUser(userId, currentUser.id);
   }
 
-  @Query(() => [User], { name: 'suggestedUsers' })
+  @Query(() => [User], { name: "suggestedUsers" })
   @UseGuards(JwtAuthGuard)
   suggestedUsers(@CurrentUser() currentUser: User) {
     return this.userService.suggestedUsers(currentUser);
   }
 
-  @Mutation(() => BaseResult, { name: 'createUserWithoutPassword' })
-  createUserWithoutPassword(@Args('data') data: CreateUserWithoutPassword) {
+  @Mutation(() => BaseResult, { name: "createUserWithoutPassword" })
+  createUserWithoutPassword(@Args("data") data: CreateUserWithoutPassword) {
     return this.userService.createUserWithoutPassword(data);
   }
 }
