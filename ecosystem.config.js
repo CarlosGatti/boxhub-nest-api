9094c892ca233c82710fca608da0c6c0afd7b99a
Eduardo Gatti <eduardo.gf@hotmail.com>
2025-11-19 21:14:14 -0700

    deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,30 @@
 module.exports = {
   apps: [
     {
-      name: 'khub',
-      script: 'start-script.sh',
+      name: "khub",
+      script: "./start-script.sh",
+      interpreter: "bash",
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
       env_homolog: {
-        NODE_ENV: 'homolog',
+        NODE_ENV: "homolog",
       },
     },
   ],
 
   deploy: {
     homolog: {
-      user: 'root',
-      host: '104.251.216.111',
-      ref: 'origin/main',
-      ssh_options: 'StrictHostKeyChecking=no',
-      path: '/var/www/khub',
+      user: "root",
+      host: "104.251.216.111",
+      ref: "origin/main",
+      ssh_options: "StrictHostKeyChecking=no",
+      path: "/var/www/khub",
     },
   },
 };
+
--- start-script.sh
+++ start-script.sh
@@ -1,1 +1,16 @@
-npm run start:prod
+#!/bin/bash
+set -e
+
+APP_DIR="/home/boxhub/boxhub-api"
+
+echo "📦  Entering application directory..."
+cd $APP_DIR
+
+echo "🛠  Installing dependencies..."
+npm install --omit=dev
+
+echo "🏗  Building project..."
+npm run build
+
+echo "🚀  Starting application..."
+exec node dist/main.js
