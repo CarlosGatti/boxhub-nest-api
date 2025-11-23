0c0c21fa08c72a3127b513844fe2f89f49039d03
Eduardo Gatti <eduardo.gf@hotmail.com>
2025-11-23 15:10:21 -0700

    fix: registrar AdminGuard no UserModule para pendingResidents

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
+import { AdminGuard } from '../auth/guards/admin.guard';
 
 @Module({
   imports: [MailModule],
-  providers: [UserService, PrismaService, UserResolver],
+  providers: [UserService, PrismaService, UserResolver, AdminGuard],
 })
 export class UserModule { }
